78a679fb033f404b4c8ef35e9c8f2d4f7968c7e8
binnaz çabuk <binnazcabuk@outlook.com>
2021-01-20 22:25:37 +0300

    MusteriManager sınıfı ayrı bir cs de olusturuldu

--- ClassMethotDemo/Musteri.cs
+++ ClassMethotDemo/Musteri.cs
@@ -14,35 +14,6 @@ namespace ClassMethotDemo
     }
 
 
-    public class MusteriManager
-    {
-        public void Ekle(Musteri musteri)
-        {
-            Console.WriteLine("  MÜŞTERİ EKLEME");
-            Console.WriteLine("********************");
-            Console.WriteLine(musteri.Ad +" "+ musteri.Soyad+" "+musteri.Id+" "+ "eklendi");
-            Console.WriteLine();
-        }
-
-        public void Sil(Musteri musteri)
-        {
-            Console.WriteLine("  MÜŞTERİ SİLME");
-            Console.WriteLine("********************");
-            Console.WriteLine(musteri.Ad + " " + musteri.Soyad+" "+"adlı" + " " + musteri.Id+" "+ "id'sine sahip müşteri silindi" );
-            Console.WriteLine();
-        }
-        public void Listele(List<Musteri> musteriler)
-        {
-            Console.WriteLine("  MÜŞTERİ LİSTESİ");
-            Console.WriteLine("********************");
-            Console.WriteLine();
-            foreach (Musteri m in musteriler)
-            {
-                Console.WriteLine("Müşteri Ad Soyad:"+" "+m.Ad+" "+ m.Soyad+" "+" Müşteri Id:"+" "+m.Id);
-              
-            }
-           
-        }
 
     }
 }
